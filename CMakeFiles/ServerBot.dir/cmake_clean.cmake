c4012747666db835d2d48579720a8eeee37b9171
Joshiepillow <joshbce123@gmail.com>
2022-01-24 18:15:22 -0600

    help me on cmake pls

---
+++ CMakeFiles/ServerBot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerBot.dir/main.cpp.o"
+  "ServerBot"
+  "ServerBot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
